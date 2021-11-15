08ad30d7ba4dad9b9c310bff530aa1f724972c62
JohnnyWiseau141 <john.a.foley141@gmail.com>
2021-11-15 11:01:42 -0500

    beginning on level up

--- src/Square.js
+++ src/Square.js
@@ -1,21 +1,11 @@
 import React from 'react';
 
-class Square extends React.Component {
-  state = {
-    value: null,
-  }
-
-  render() {
-    return (
-      <button 
-        className="square" 
-                          // single quotes won't work below
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
